857d28d25fde136c653fd345662fcc6fa8061bf0
Sonu Chauhan <77970439+Mrxsonux007@users.noreply.github.com>
2022-03-24 18:29:58 +0530

    second

--- Online_Nursery_Shop/settings.py
+++ Online_Nursery_Shop/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT =os.path.join(BASE_DIR,'media')
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles','media')
 
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
