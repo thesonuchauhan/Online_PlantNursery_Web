f16f3dd828ebcf4f8acb3bc0afcd59a9cb93db74
Sonu Chauhan <77970439+Mrxsonux007@users.noreply.github.com>
2022-03-24 22:29:26 +0530

    Update settings.py

--- Online_Nursery_Shop/settings.py
+++ Online_Nursery_Shop/settings.py
@@ -110,7 +110,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR,'media', 'staticfiles')
+]
