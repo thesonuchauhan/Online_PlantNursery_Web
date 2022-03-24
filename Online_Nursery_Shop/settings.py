4c7e562f301f73a184cbb3254fdf0dfee6fcd60a
Sonu Chauhan <77970439+Mrxsonux007@users.noreply.github.com>
2022-03-24 22:56:11 +0530

    Update settings.py

--- Online_Nursery_Shop/settings.py
+++ Online_Nursery_Shop/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 STATICFILES_DIR = [
